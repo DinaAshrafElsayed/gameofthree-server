cbfa8dea6b283c72858990a250f4bfab8edb208d
Dina <dina.elsayed@vodafone.com>
2019-11-21 11:05:45 +0200

    adding more todo's in service of play game

--- src/main/java/takeaway/server/gameofthree/service/GameService.java
+++ src/main/java/takeaway/server/gameofthree/service/GameService.java
@@ -7,10 +7,10 @@ public class GameService {
 		 * get sender
 		 * make sure it's his turn
 		 * make sure game is still in progress no winner announced yet( value !=1)
-		 * validate input from previous one (matches (-1,0,1) and divisible by 3
-		 * get other player
+		 * validate input from previous one (matches rule (-1,0,1)) and divisible by 3
+		 * get other player and make sure he is still online
 		 * communicate with other player and send data to him
-		 *
+		 * in case of win I should notify both players
 		 */
 	}
 
