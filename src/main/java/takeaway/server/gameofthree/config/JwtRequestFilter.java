1a6c93d811dd4eb364bc5a539d657394fdb5ab6a
Dina Ashraf Samir Elsayed <dina.ashrafelsayed@gmail.com>
2019-11-23 19:07:59 +0200

    fixing indentation

--- src/main/java/takeaway/server/gameofthree/config/JwtRequestFilter.java
+++ src/main/java/takeaway/server/gameofthree/config/JwtRequestFilter.java
@@ -23,7 +23,7 @@ import takeaway.server.gameofthree.util.JwtTokenUtil;
 public class JwtRequestFilter extends OncePerRequestFilter {
 	@Autowired
 	private JwtTokenUtil jwtTokenUtil;
-	
+
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
 
@@ -53,17 +53,17 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 			// if token is valid configure Spring Security to manually set
 			// authentication
 			if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-			userDetails, null, userDetails.getAuthorities());
-			usernamePasswordAuthenticationToken
-			.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-			// After setting the Authentication in the context, we specify
-			// that the current user is authenticated. So it passes the
-			// Spring Security Configurations successfully.
-			SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken
+						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// After setting the Authentication in the context, we specify
+				// that the current user is authenticated. So it passes the
+				// Spring Security Configurations successfully.
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 			}
-			}
-			chain.doFilter(request, response);
-			}
+		}
+		chain.doFilter(request, response);
+	}
 
 }
